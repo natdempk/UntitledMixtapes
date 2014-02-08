1a7a4629099e10bee7e2e9c7d78c05219edcdbc7
Nat Dempkowski <dempkowski.n@husky.neu.edu>
2014-02-08 04:52:51 -0500

    add frontend

---
+++ playlist.py
@@ -0,0 +1,22 @@
+from flask import Flask, request, session, g, redirect, url_for, \
+     abort, render_template, flash
+
+
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+@app.route('/')
+def show_home():
+    return render_template('index.html')
+
+@app.route('/generate', methods=['POST'])
+def generate_playlist():
+    artist = request.args.get('artist')
+    bpm = request.args.get('bpm')
+    diversity = request.args.get('diversity')
+    tracks = request.args.get('tracks')
+
+    return render_template('playlist.html', playlist_id=playlist_id)
+
+if __name__ == '__main__':
+    app.run()
