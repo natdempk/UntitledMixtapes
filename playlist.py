2164674f12c8526d4d86b4a48911d94608347f94
Nat Dempkowski <dempkowski.n@husky.neu.edu>
2014-02-08 11:09:54 -0500

    fix getting form arguments

--- playlist.py
+++ playlist.py
@@ -7,7 +7,7 @@ import pyechonest
 app = Flask(__name__)
 app.config.from_object(__name__)
 
-app.config['PROPAGATE_EXCEPTIONS'] = True
+#app.config['PROPAGATE_EXCEPTIONS'] = True
 
 @app.route('/')
 def show_home():
@@ -15,19 +15,20 @@ def show_home():
 
 @app.route('/generate', methods=['POST'])
 def generate_playlist():
-    artist = request.args.get('artist')
-    track = request.args.get('track')
-    #try:
+    artist = request.form['artist']
+    track = request.form['track']
     tuples = query.do_everything(artist, track)
-    #except EchoNestAPIError as e: # doesn't work fuck
-    #    return render_template('ratelimit.html')
-    bpm = request.args.get('bpm')
-    diversity = request.args.get('diversity')
-    tracks = request.args.get('tracks')
+    diversity = request.form['diversity']
+    tracks = request.form['tracks']
+    #print artist
+    #print track
+    #print bpm
+    #print diversity
+    #print tracks
     playlist_id = 'test'
     #print tuples
-
     return render_template('playlist.html', playlist_id=playlist_id, tuples=tuples)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    #app.run(debug=True)
+    app.run()
