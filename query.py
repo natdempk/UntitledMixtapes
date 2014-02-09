98ad2f256bea59b63ff6dab4c05aac55f220f777
Zack <zdhickman@gmail.com>
2014-02-09 04:34:04 -0500

    fix margin, fix related artist not found

--- query.py
+++ query.py
@@ -16,11 +16,14 @@ class echoArtistThread(threading.Thread):
 
     def run(self):
         while True:
-            a = self.queue.get()
-            ar = artist.Artist(a)
-            l = ar.get_songs()
-            self.artist_list.append({'artist':ar,'songs':l})
-            self.queue.task_done()
+            try:
+                a = self.queue.get()
+                ar = artist.Artist(a)
+                l = ar.get_songs()
+                self.artist_list.append({'artist':ar,'songs':l})
+                self.queue.task_done()
+            except:
+                self.queue.task_done()
 
 class echoThread(threading.Thread):
     def __init__(self, queue, songs_list):
@@ -30,9 +33,12 @@ class echoThread(threading.Thread):
 
     def run(self):
         while True:
-            s = self.queue.get() # get artist from queue
-            self.songs_list.append(s.get_audio_summary())
-            self.queue.task_done()
+            try:
+                s = self.queue.get() # get artist from queue
+                self.songs_list.append(s.get_audio_summary())
+                self.queue.task_done()
+            except:
+                self.queue.task_done()
 
             #the_artist = artist.Artist(a)
             #songs = the_artist.get_songs()[:num_songs]
